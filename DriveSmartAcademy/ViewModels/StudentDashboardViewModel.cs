d5210ece789f44227dc21419d8075ef0886e571c
aaaBbbb01 <abdulrahmanbasheeb31@gmail.com>
2025-03-24 23:21:41 +0800

    Add files via upload

--- DriveSmartAcademy/ViewModels/StudentDashboardViewModel.cs
+++ DriveSmartAcademy/ViewModels/StudentDashboardViewModel.cs
@@ -7,18 +7,11 @@ namespace DriveSmartAcademy.ViewModels
     {
         public string StudentName { get; set; }
         public string StudentEmail { get; set; }
-        public List<CourseViewModel> EnrolledCourses { get; set; }
-        public List<QuizResultViewModel> RecentQuizResults { get; set; }
-        public FeedbackViewModel StudentFeedback { get; set; }
-        public List<ExamViewModel> DrivingTheoryExams { get; set; }
+        public ICollection<CourseViewModel> EnrolledCourses { get; set; }
+        public ICollection<QuizResultViewModel> RecentQuizResults { get; set; }
+        public ICollection<ExamViewModel> DrivingTheoryExams { get; set; }
         public CourseProgressViewModel OverallProgress { get; set; }
-
-        public StudentDashboardViewModel()
-        {
-            EnrolledCourses = new List<CourseViewModel>();
-            RecentQuizResults = new List<QuizResultViewModel>();
-            DrivingTheoryExams = new List<ExamViewModel>();
-        }
+        public FeedbackViewModel StudentFeedback { get; set; }
     }
 
     public class CourseViewModel
@@ -26,14 +19,9 @@ namespace DriveSmartAcademy.ViewModels
         public int CourseId { get; set; }
         public string CourseName { get; set; }
         public string Description { get; set; }
-        public int CompletionPercentage { get; set; }
+        public decimal CompletionPercentage { get; set; }
         public DateTime EnrollmentDate { get; set; }
-        public List<ModuleViewModel> Modules { get; set; }
-
-        public CourseViewModel()
-        {
-            Modules = new List<ModuleViewModel>();
-        }
+        public ICollection<ModuleViewModel> Modules { get; set; }
     }
 
     public class ModuleViewModel
@@ -41,20 +29,6 @@ namespace DriveSmartAcademy.ViewModels
         public int ModuleId { get; set; }
         public string ModuleName { get; set; }
         public bool IsCompleted { get; set; }
-        public List<QuizViewModel> Quizzes { get; set; }
-
-        public ModuleViewModel()
-        {
-            Quizzes = new List<QuizViewModel>();
-        }
-    }
-
-    public class QuizViewModel
-    {
-        public int QuizId { get; set; }
-        public string QuizName { get; set; }
-        public bool IsCompleted { get; set; }
-        public int? Score { get; set; }
     }
 
     public class QuizResultViewModel
@@ -66,30 +40,20 @@ namespace DriveSmartAcademy.ViewModels
         public string CourseName { get; set; }
     }
 
-    public class FeedbackViewModel
-    {
-        public int FeedbackId { get; set; }
-        public string FeedbackText { get; set; }
-        public DateTime FeedbackDate { get; set; }
-        public string InstructorResponse { get; set; }
-        public DateTime? ResponseDate { get; set; }
-    }
-
     public class ExamViewModel
     {
         public int ExamId { get; set; }
         public string ExamName { get; set; }
         public DateTime ExamDate { get; set; }
         public bool IsPassed { get; set; }
-        public int? Score { get; set; }
     }
 
     public class CourseProgressViewModel
     {
+        public decimal OverallPercentage { get; set; }
         public int CompletedModules { get; set; }
         public int TotalModules { get; set; }
         public int CompletedQuizzes { get; set; }
         public int TotalQuizzes { get; set; }
-        public double OverallPercentage { get; set; }
     }
 }
